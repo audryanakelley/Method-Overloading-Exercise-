561f7c566d6a0d921df78ac5b76e34facc69edc0
Audryana Kelley <audryanakelley@gmail.com>
2024-07-15 15:07:22 -0500

    made update

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MethodOverloading
+﻿using System.Net;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            var sum = x + y;
+
+            if (isTrue == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isTrue == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+
+            return "done";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Console.WriteLine(Add(2, 2)); 
+           // Console.WriteLine(Add(4.0m, 8.0m)); 
+           int x = 32;
+           int y = 38;
         }
     }
 }
